21b0d75dcea6268239667a54d2215d920587868b
Jameel Qureshi <jameelqureshi2013@gmail.com>
2020-04-17 11:16:41 +0500

    Mirror Problem Fixed

---
+++ Assets/Scripts/CameraProjectionAdjuster.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraProjectionAdjuster : MonoBehaviour
+{
+    private Camera m_camera;
+    void OnPreCull()
+    {
+        m_camera = GetComponent<Camera>();
+        m_camera.ResetWorldToCameraMatrix();
+        m_camera.ResetProjectionMatrix();
+        m_camera.projectionMatrix = m_camera.projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1));
+    }
+
+    void OnPreRender()
+    {
+        GL.SetRevertBackfacing(true);
+    }
+
+    void OnPostRender()
+    {
+        GL.SetRevertBackfacing(false);
+    }
+
+}
