5aabb9b07bd7efcfd4979e600f2ed92c45631697
Jameel Qureshi <jameelqureshi2013@gmail.com>
2020-04-30 12:24:00 +0500

    Countdown Timer
    Countdown Timer added and timer fill bar

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Timers;
+using System;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour {
+
+    [Header("DisplayArea")]
+	public Text timerText;
+    public Image timerFill;
+
+
+	public UnityEvent onTimeEnd;
+
+	private float currentMaxTime;
+	private float currentTime;
+    private int secondsSpent;
+    private int milisecondsSpent;
+    bool isCountingDown;
+	DateTime startTime;
+	
+	
+
+	void Update()
+	{
+		if (isCountingDown)
+			UpdateCounter();
+	}
+
+	public void StartTimer(float time)
+	{
+		startTime=System.DateTime.Now;
+		currentTime = time;
+		currentMaxTime = time;
+		isCountingDown = true;
+
+	}
+
+    private void Start()
+    {
+        StartTimer(30.0f);
+    }
+
+
+    void UpdateCounter()
+	{
+		DateTime newTime = System.DateTime.Now;
+		TimeSpan difference = newTime.Subtract(startTime);
+
+		secondsSpent = difference.Seconds;
+        milisecondsSpent = (secondsSpent*1000)+difference.Milliseconds;
+        Debug.Log("Time spent in miliseconds: "+milisecondsSpent);
+        Debug.Log("Time spent in seconds: " + secondsSpent);
+
+        currentTime = currentMaxTime - secondsSpent;
+		if (currentTime <= 0)
+		{
+				isCountingDown = false;
+				currentTime = 0;
+            onTimeEnd.Invoke();
+		}
+		DisplayTime();
+       
+
+	}
+
+	
+
+	void DisplayTime()
+	{	int min;
+		float sec;
+		float displayTimer = Mathf.Round (currentTime);
+		min = (int)(displayTimer / 60);
+		sec = (int)(displayTimer % 60);
+		if(sec<10)
+			timerText.text = "0"+sec;
+		else
+			timerText.text = ""+sec;
+
+        if (timerFill!=null)
+        {
+            timerFill.fillAmount =  milisecondsSpent / (currentMaxTime*1000);
+        }
+    }
+	public void Pause(){
+		currentMaxTime = currentTime;
+		isCountingDown = false;
+
+	}
+	public void Resume(){
+		isCountingDown = true;
+		startTime=System.DateTime.Now;
+	}
+}
